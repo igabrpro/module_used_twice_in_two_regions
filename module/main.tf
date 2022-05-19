94c38644fe57dc174173a3e16a42b62d2e51d6e5
Ivan Gabrovski <igabrpro88@gmail.com>
2022-05-19 14:06:00 +0300

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -9,15 +9,8 @@ terraform {
 
 variable "Ec2_ami"{}
 
-variable "AWS_ACCESS_KEY_ID" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
-variable "AWS_SESSION_TOKEN" {}
-
 
 resource "aws_instance" "server" {
   ami           = var.Ec2_ami
   instance_type = "t2.micro"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-  token = var.AWS_SESSION_TOKEN
 }
